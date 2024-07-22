70b9952ba3f4736197eca9dc83e0a13bf6fbf28c
Alascose <145700209+Alascose@users.noreply.github.com>
2024-07-22 02:45:28 +0000

    Ajout du script event.js et modification des fichier projet et du style.css

---
+++ css/event.js
@@ -0,0 +1,31 @@
+var buttons = document.querySelectorAll('.indexli');
+
+// Fonction pour enregistrer l'index du bouton actif dans le stockage local
+function setActiveButton(index) {
+  localStorage.setItem('activeButtonIndex', index);
+}
+
+// Fonction pour obtenir l'index du bouton actif depuis le stockage local
+function getActiveButton() {
+  return localStorage.getItem('activeButtonIndex');
+}
+
+// Ajouter un écouteur d'événement à chaque bouton
+buttons.forEach(function(button, index) {
+  button.addEventListener('click', function() {
+    // Supprimer la classe 'active' de tous les boutons
+    buttons.forEach(function(btn) {
+      btn.classList.remove('active');
+    });
+    // Ajouter la classe 'active' uniquement au bouton cliqué
+    button.classList.add('active');
+    // Enregistrer l'index du bouton cliqué dans le stockage local
+    setActiveButton(index);
+  });
+});
+
+// Récupérer l'index du bouton actif depuis le stockage local
+var activeButtonIndex = getActiveButton();
+if (activeButtonIndex !== null) {
+  buttons[activeButtonIndex].classList.add('active');
+}
